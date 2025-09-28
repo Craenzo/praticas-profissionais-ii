058933b5789bcb1fe9c085acbc4fcacf8b2cd975
Davi <davvifsantos@gmail.com>
2025-09-28 15:15:50 -0300

    Animação de Costas e Iluminação

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
-@onready var anim = $AnimatedSprite2D  # AnimatedSprite2D com 4 animações: "frente", "costas", "direita", "esquerda"
+@onready var anim = $AnimatedSprite2D 
 
 var base_y = 0.0
 var walk_cycle = 0.0
@@ -9,7 +9,7 @@ var walk_cycle = 0.0
 func _ready():
 	base_y = anim.position.y
 	anim.play("frente")
-	anim.speed_scale = 1.0  # velocidade da animação
+	anim.speed_scale = 1.0  
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -22,23 +22,23 @@ func _physics_process(delta):
 	velocity = input_vector * speed
 	move_and_slide()
 	
-	# Escolhe animação baseada na direção
-	if input_vector.y > 0:  # andando pra frente
-		anim.play("frente")  # força tocar a animação
+	
+	if input_vector.y > 0:  
+		anim.play("frente") 
 		walk_cycle += delta * 18.0
-		anim.position.y = base_y + sin(walk_cycle) * 1.0  # quique frontal
-	elif input_vector.y < 0:  # andando pra trás
+		anim.position.y = base_y + sin(walk_cycle) * 1.5 
+	elif input_vector.y < 0:  
 		anim.play("costas")
-		anim.position.y = base_y
-		walk_cycle = 0.0
-	elif input_vector.x > 0:  # andando pra direita
+		anim.position.y = base_y + sin(walk_cycle) * 1.5 
+		walk_cycle += delta * 18.0
+	elif input_vector.x > 0:  
 		anim.play("direita")
-		anim.position.y = base_y
-		walk_cycle = 0.0
-	elif input_vector.x < 0:  # andando pra esquerda
+		anim.position.y = base_y + sin(walk_cycle) * 1.5
+		walk_cycle += delta * 18.0
+	elif input_vector.x < 0:  
 		anim.play("esquerda")
-		anim.position.y = base_y
-		walk_cycle = 0.0
+		anim.position.y = base_y + sin(walk_cycle) * 1.5
+		walk_cycle += delta * 18.0
 	else:
 		# parado
 		anim.stop()
