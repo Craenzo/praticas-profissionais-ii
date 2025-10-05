b3020fe9eff75237f0becffe1b2fda85d16f4ae3
Enzo Rodrigues <enzo.rodrigues62@aluno.ifce.edu.br>
2025-10-05 20:11:23 -0300

    100% integrado o trabalho do franco e do superenzo

---
+++ Script/menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+func _ready() -> void:
+	for node in get_tree().get_nodes_in_group("button"):
+	   
+		if node is Button:
+			node.connect("pressed", Callable(self, "on_button_pressed").bind(node))
+		if node.has_signal("mouse_entered"):
+			node.connect("mouse_entered", Callable(self, "mouse_interaction").bind(node, "entered"))
+		if node.has_signal("mouse_exited"):
+			node.connect("mouse_exited", Callable(self, "mouse_interaction").bind(node, "exited"))
+
+
+#quando um botão é pressionado
+func on_button_pressed(button: Button) -> void:
+	match button.name:
+		"Jogar":
+			var err = get_tree().change_scene_to_file("res://Cenas/player.tscn")
+			if err != OK:
+				push_error("Falha ao trocar para player.tscn: %s" % str(err))
+		"Saves":
+			# ajuste o caminho para a cena de saves se tiver uma
+			var err2 = get_tree().change_scene_to_file("")
+			if err2 != OK:
+				push_error("Falha ao trocar para saves.tscn: %s" % str(err2))
+		"Config":
+			var err3 = get_tree().change_scene_to_file("")
+			if err3 != OK:
+				push_error("Falha ao trocar para config.tscn: %s" % str(err3))
+		"Thegamers":
+			# abre link externo (substitua se quiser outro URL)
+			OS.shell_open("https://www.youtube.com/@corinthians")
+		"quit":
+			get_tree().quit()
